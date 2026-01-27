79cecebdf620b98ed6be3bcba7e41d84ee122f38
Ahmed Al Saad <ahmed.q2099@gmail.com>
2026-01-27 13:17:37 -0500

    Initial architecture of crazy 8s assignment

--- src/main/java/edu/kennesaw/crazy8s/Main.java
+++ src/main/java/edu/kennesaw/crazy8s/Main.java
@@ -1,17 +1,20 @@
 package main.java.edu.kennesaw.crazy8s;
 
+import main.java.edu.kennesaw.crazy8s.cardDeck.Deck;
+import main.java.edu.kennesaw.crazy8s.game.CrazyEightsGame;
+import main.java.edu.kennesaw.crazy8s.player.CpuPlayer;
+import main.java.edu.kennesaw.crazy8s.player.HumanPlayer;
+import main.java.edu.kennesaw.crazy8s.player.Player;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        /*
+        Player human = new HumanPlayer("You");
+        Player cpu = new CpuPlayer("CPU");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Deck deck = Deck.createStandardShuffled();
+        CrazyEightsGame game = new CrazyEightsGame(deck, human, cpu);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        game.run();
+        */
     }
 }
---
+++ src/main/java/edu/kennesaw/crazy8s/game/TurnAction.java
@@ -0,0 +1,4 @@
+package main.java.edu.kennesaw.crazy8s.game;
+
+public class TurnAction {
+}
