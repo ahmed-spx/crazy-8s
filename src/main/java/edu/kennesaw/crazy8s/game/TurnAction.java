dacf7c8d0249a93e1b6d55e01ab1d89be4c149d5
Ahmed Al Saad <ahmed.q2099@gmail.com>
2026-02-04 18:58:13 -0500

    Constructor for TurnAction

--- src/main/java/edu/kennesaw/crazy8s/game/TurnAction.java
+++ src/main/java/edu/kennesaw/crazy8s/game/TurnAction.java
@@ -1,10 +1,16 @@
 package main.java.edu.kennesaw.crazy8s.game;
 
-public class TurnAction {
+import main.java.edu.kennesaw.crazy8s.cards.StandardCard;
+import main.java.edu.kennesaw.crazy8s.player.Player;
 
-    void takeTurn(TurnsContext context){
+import javax.swing.text.PlainDocument;
+
+public class TurnAction {
+        //constructor that will take current card, and current player
+        //do-while loop that will have an "end game" method or class
+        //end game will be if either player has no cards, or no moves can be made and there's no deck left
+    public TurnAction(StandardCard currentCard, Player currentPlayer){
 
     }
-
 }
 
