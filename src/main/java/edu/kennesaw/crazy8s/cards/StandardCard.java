60a58df35742c3c9b4716a4368d8a85c74438ba1
Ahmed Al Saad <ahmed.q2099@gmail.com>
2026-02-18 17:52:05 -0500

    updated rank and suits, constructor for standard card

--- src/main/java/edu/kennesaw/crazy8s/cards/StandardCard.java
+++ src/main/java/edu/kennesaw/crazy8s/cards/StandardCard.java
@@ -1,11 +1,19 @@
 package main.java.edu.kennesaw.crazy8s.cards;
+import main.java.edu.kennesaw.crazy8s.domain.Rank;
+import main.java.edu.kennesaw.crazy8s.domain.Suit;
+
 
 public class StandardCard implements Card {
-    private String card;
+    protected String card;
+    protected Rank rank;
+    protected Suit suit;
 
     @Override
     public String getCard() {
         return card;
     }
-
+    public StandardCard standardCard(Rank rank, Suit suit){
+        this.rank = rank;
+        this.suit = suit;
+    }
 }
